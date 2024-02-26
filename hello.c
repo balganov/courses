7d0178392669ed5bc0575a282694ed55b3315fa3
balganov <160471135+balganov@users.noreply.github.com>
2024-02-26 09:01:49 +0000

    Mon, Feb 26, 2024, 3:01 PM +06:00

--- hello.c
+++ hello.c
@@ -4,5 +4,16 @@
 int main(void)
 {
     int x = get_int("type first int ");
-    printf("your first int %i",x);
+    int y = get_int("type second int ");
+
+    if (x > y)
+    {
+        printf("first is greater than second");
+    } else if (x < y)
+    {
+        printf("second is greater than first");
+    } else
+    {
+        printf("first is equal to second");
+    }
 }
