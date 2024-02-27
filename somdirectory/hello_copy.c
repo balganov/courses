6a72fcfdd8b7a39745e8db664862169d90f6416c
balganov <160471135+balganov@users.noreply.github.com>
2024-02-27 11:08:08 +0000

    Tue, Feb 27, 2024, 5:08 PM +06:00

---
+++ somdirectory/hello_copy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int x = get_int("type first int ");
+    int y = get_int("type second int ");
+
+    while (true)
+    {
+        printf("aaaaa\n");
+    }
+
+}
