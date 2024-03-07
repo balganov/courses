d2d5cc5b3547229b5b97e8e1c7711ea22f9acdf8
balganov <160471135+balganov@users.noreply.github.com>
2024-03-07 15:38:50 +0000

    Thu, Mar 7, 2024, 8:38 PM +05:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -20,13 +20,17 @@ int main(void)
 
 void draw (int a)
 {
-    for (int r = a; r > 0; r--)
+    for (int r = 0; r < a; r++)
     {
+        for (int c = 0; c < a; c++)
+        {
+            printf("#");
+        }
+
         for (int c = 0; c < r; c++)
         {
             printf("#");
         }
-        
         printf("\n");
     }
 }
