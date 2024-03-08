a2a4d86c12fda80080714935b0bcb671feb3ed1a
balganov <160471135+balganov@users.noreply.github.com>
2024-03-08 16:21:11 +0000

    Fri, Mar 8, 2024, 9:21 PM +05:00

---
+++ cash/cash.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int n;
+    do
+    {
+       n = get_int("Change owed: ");
+    }
+    while(n < 0);
+    //25, 10, 5, 1
+    int coins;
+    if (n / 25 && n % 25 == 0)
+    {
+        coins = n / 25;
+        printf("%i\n",coins)
+    }
+
+    int k = n % 25;
+    //printf("%i\n",k);
+}
