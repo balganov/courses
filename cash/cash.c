99d39c8ecd3e07a3b347f936f8deaaf5b44b53ce
balganov <160471135+balganov@users.noreply.github.com>
2024-03-11 13:50:22 +0000

    Mon, Mar 11, 2024, 6:50 PM +05:00

--- cash/cash.c
+++ cash/cash.c
@@ -12,9 +12,24 @@ int main(void)
     //25, 10, 5, 1
     int coins = 0;
 
-    while (n > 25)
+    while (n >= 25)
     {
         n = n - 25;
+        coins++;
+    }
+    while (n >= 10)
+    {
+        n = n - 10;
+        coins++;
+    }
+    while (n >= 5)
+    {
+        n = n - 5;
+        coins++;
+    }
+    while (n >= 1)
+    {
+        n = n - 1;
         coins++;
     }
 
