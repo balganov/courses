b087c4198da7214cbdf4039e4e4011c78f1f3292
balganov <160471135+balganov@users.noreply.github.com>
2024-03-11 14:26:56 +0000

    Mon, Mar 11, 2024, 7:26 PM +05:00

--- credit/credit.c
+++ credit/credit.c
@@ -11,15 +11,17 @@ int main(void)
     }
     while (card_number < 0);
 
-    int last_digit = card_number % 10;
+    int digit = card_number % 10;
+    int sum;
 
     100
     10000
     1000000
     100000000
-    for (int i = 0; i < 1000000; i*100)
+    for (int i = 10; i < 1000000; i*10)
     {
-        
+        digit = card_number % i;
+        sum = digit + digit*2
     }
 
     printf("%i\n",last_digit);
