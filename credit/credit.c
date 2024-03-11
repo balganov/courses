df24e8024dc6b1c1c3161b1ef9599a8e9dacf60f
balganov <160471135+balganov@users.noreply.github.com>
2024-03-11 14:10:24 +0000

    Mon, Mar 11, 2024, 7:10 PM +05:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,5 +3,19 @@
 
 int main(void)
 {
+    long card;
+
+    do
+    {
+        card = get_long("Number: ");
+    }
+    while (card < 0);
+
     
+    string output = "INVALID\n";
+    output = "AMEX\n";
+    output = "VISA\n";
+    output = "MASTERCARD\n";
+
+
 }
