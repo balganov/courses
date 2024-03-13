6c2540b1abbc0a5d2bac26abb74e4d2e38006363
balganov <160471135+balganov@users.noreply.github.com>
2024-03-13 06:56:14 +0000

    Wed, Mar 13, 2024, 11:56 AM +05:00

--- credit/credit.c
+++ credit/credit.c
@@ -22,7 +22,7 @@ int main(void)
     for (long i = 10; i < 1000000000000000000; i=i*100)
     {
         second_to_last = (card_number/i % 10)*2;
-        printf("second to last %i\n",second_to_last);
+        //printf("second to last %i\n",second_to_last);
         if (second_to_last < 9)
         {
             sum = sum + (card_number/i % 10)*2;
@@ -30,13 +30,13 @@ int main(void)
         {
             sum = sum + second_to_last%10 + 1;
         }
-        printf("sum inside first loop %i\n",sum);
+        //printf("sum inside first loop %i\n",sum);
     }
 
     for (long i = 100; i < 10000000000000000; i=i*100)
     {
         last = last + card_number/i % 10;
-        printf("last inside second loop %i\n",last);
+        //printf("last inside second loop %i\n",last);
     }
 
     if ((sum + last) % 10 == 0)
