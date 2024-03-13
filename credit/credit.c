e94daa73bb7714b902f13059ec81f5736c713af0
balganov <160471135+balganov@users.noreply.github.com>
2024-03-13 07:30:05 +0000

    Wed, Mar 13, 2024, 12:30 PM +05:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,9 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
 int get_length(long a);
-int get_first_digit (long a);
-int get_first_two_digits (long a);
-
+int get_first_digit(long a);
+int get_first_two_digits(long a);
 
 int main(void)
 {
@@ -21,25 +20,22 @@ int main(void)
     int length = get_length(card_number);
     int first_digit = get_first_digit(card_number);
     int first_two_digits = get_first_two_digits(card_number);
-    //5555555555554444
-    for (long i = 10; i < 1000000000000000000; i=i*100)
+    for (long i = 10; i < 1000000000000000000; i = i * 100)
     {
-        second_to_last = (card_number/i % 10)*2;
-        //printf("second to last %i\n",second_to_last);
+        second_to_last = (card_number / i % 10) * 2;
         if (second_to_last < 9)
         {
-            sum = sum + (card_number/i % 10)*2;
-        } else
-        {
-            sum = sum + second_to_last%10 + 1;
+            sum = sum + (card_number / i % 10) * 2;
         }
-        //printf("sum inside first loop %i\n",sum);
+        else
+        {
+            sum = sum + second_to_last % 10 + 1;
+        }
     }
 
-    for (long i = 100; i < 10000000000000000; i=i*100)
+    for (long i = 100; i < 10000000000000000; i = i * 100)
     {
-        last = last + card_number/i % 10;
-        //printf("last inside second loop %i\n",last);
+        last = last + card_number / i % 10;
     }
 
     if ((sum + last) % 10 == 0)
@@ -47,20 +43,25 @@ int main(void)
         if ((first_two_digits == 34 || first_two_digits == 37) && length == 15)
         {
             printf("AMEX\n");
-        } else if (first_digit == 4 && (length == 13 || length == 16))
+        }
+        else if (first_digit == 4 && (length == 13 || length == 16))
         {
             printf("VISA\n");
-        } else if (first_two_digits >= 51 && first_two_digits <=55 && length == 16)
+        }
+        else if (first_two_digits >= 51 && first_two_digits <= 55 && length == 16)
         {
             printf("MASTERCARD\n");
-        } else printf("INVALID\n");
-    } else
+        }
+        else
+            printf("INVALID\n");
+    }
+    else
     {
         printf("INVALID\n");
     }
 }
 
-int get_length (long a)
+int get_length(long a)
 {
     int i = 1;
     while (a > 9)
@@ -70,7 +71,7 @@ int get_length (long a)
     }
     return i;
 }
-int get_first_digit (long a)
+int get_first_digit(long a)
 {
     while (a > 9)
     {
@@ -78,7 +79,7 @@ int get_first_digit (long a)
     }
     return a;
 }
-int get_first_two_digits (long a)
+int get_first_two_digits(long a)
 {
     while (a > 90)
     {
