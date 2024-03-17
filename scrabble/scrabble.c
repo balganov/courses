ff50425c4ff15665d2d461cf4b34416b950f2860
balganov <160471135+balganov@users.noreply.github.com>
2024-03-17 15:09:17 +0000

    Sun, Mar 17, 2024, 8:09 PM +05:00

---
+++ scrabble/scrabble.c
@@ -0,0 +1,34 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+int points (string word);
+
+int main(void)
+{
+    string player1, player2;
+
+
+    player1 = get_string("Player 1: ");
+    player2 = get_string("Player 2: ");
+
+    points(player1);
+    points(player2);
+}
+
+int points (string word)
+{
+    string p1 = "AEILNORSTU";
+    string p2 = "DG";
+    string p3 = "CMP";
+    string p4 = "FHVWY";
+    string p5 = "K";
+    string p8 = "JX";
+    string p10 = "QZ";
+
+    for (int i = 0; i < strlen(word); i++)
+    {
+        tolower(word[i])
+    }
+    return 0;
+}
