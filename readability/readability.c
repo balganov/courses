49ceaf4e5a61b90bc32cf0b444333b279b40b299
balganov <160471135+balganov@users.noreply.github.com>
2024-03-18 06:20:18 +0000

    Mon, Mar 18, 2024, 11:20 AM +05:00

--- readability/readability.c
+++ readability/readability.c
@@ -11,4 +11,11 @@ int main(void)
 void calculate_index(string t)
 {
     int length = strlen(t);
+    int letters = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        
+        if (t[i] >= 'A' && t[i] <= 'Z')
+    }
 }
