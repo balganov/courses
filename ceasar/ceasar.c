0ebf64f1db28c77c59f38c98894650d6ac0c1773
balganov <160471135+balganov@users.noreply.github.com>
2024-03-18 12:11:39 +0000

    Mon, Mar 18, 2024, 5:11 PM +05:00

--- ceasar/ceasar.c
+++ ceasar/ceasar.c
@@ -5,8 +5,8 @@
 int main(int argc, string argv[])
 {
     int key = atoi(argv[1]);
-    
-    if (argc != 2 || key < 0)
+
+    if (argc != 2 || key <= 0)
     {
         printf("Usage ./ceasar key\n");
         return 1;
