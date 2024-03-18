965e90bee80baf642427a833ad05a09f1b9d1f6b
balganov <160471135+balganov@users.noreply.github.com>
2024-03-18 12:01:43 +0000

    Mon, Mar 18, 2024, 5:01 PM +05:00

--- ceasar/ceasar.c
+++ ceasar/ceasar.c
@@ -1,13 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(argc, argv[])
+int main(int argc, string argv[])
 {
     int key;
 
-    if (argc != 2)
+    if (argc != 2 || argv[1] < 0)
     {
         printf("Usage ./ceasar key\n");
         return 1;
     }
+    printf("all good %s", argv[1]);
 }
