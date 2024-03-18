b8498b80d33d3db680ff6ea97a20342a75a46200
balganov <160471135+balganov@users.noreply.github.com>
2024-03-18 06:26:20 +0000

    Mon, Mar 18, 2024, 11:26 AM +05:00

--- readability/readability.c
+++ readability/readability.c
@@ -15,6 +15,7 @@ void calculate_index(string t)
 {
     int length = strlen(t);
     int letters = 0;
+    int words = 0;
 
     for (int i = 0; i < length; i++)
     {
@@ -23,6 +24,11 @@ void calculate_index(string t)
         {
             letters += 1;
         }
+        if (t[i] == ' ')
+        {
+            words += 1;
+        }
     }
     printf("number of letters %i\n",letters);
+    printf("number of words %i\n",words+1);
 }
