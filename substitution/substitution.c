4ccef80674ad65e2c15327a747c46ec4cfe820e2
balganov <160471135+balganov@users.noreply.github.com>
2024-03-21 07:19:13 +0000

    Thu, Mar 21, 2024, 12:19 PM +05:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -19,6 +19,7 @@ int main(int argc, string argv[])
             if (!isalpha(input[i]) || strchr("ABCDEFGHIJKLMNOPQRSTUVWXYZ",input[i]) == NULL)
             {
                 alpha = false;
+                break;
             }
         }
     }
