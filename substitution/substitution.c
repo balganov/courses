269854151fcb61038190698879b8db98fb1d682e
balganov <160471135+balganov@users.noreply.github.com>
2024-03-21 07:43:03 +0000

    Thu, Mar 21, 2024, 12:43 PM +05:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -13,9 +13,10 @@ int main(int argc, string argv[])
     if (argc == 2)
     {
         l = strlen(argv[1]);
-        input = toupper(argv[1]);
+        input = argv[1];
         for (int i = 0; i < l; i++)
         {
+            input[i] = toupper(input[i]);
             if (!isalpha(input[i]) || strchr("ABCDEFGHIJKLMNOPQRSTUVWXYZ",input[i]) == NULL)
             {
                 alpha = false;
