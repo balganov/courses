8d177a2e3a58bce1e86d20e2a9a99ceda7b7dc3f
balganov <160471135+balganov@users.noreply.github.com>
2024-03-21 06:02:53 +0000

    Thu, Mar 21, 2024, 11:02 AM +05:00

---
+++ substitution/substitution.c
@@ -0,0 +1,34 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void cipher(string s, int k);
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage ./ceasar key\n");
+        return 1;
+    }
+    int l = strlen(argv[1]);
+    string input = argv[1];
+    for (int i = 0; i < l; i++)
+    {
+        if (input[0] == '0' || !isdigit(input[i]))
+        {
+            printf("Usage ./ceasar key\n");
+            return 1;
+        }
+    }
+
+    cipher(get_string("plaintext:  "), atoi(input));
+
+    return 0;
+}
+
+void cipher(string s, int k)
+{
+    printf("ciphertext: %s\n", s);
+}
