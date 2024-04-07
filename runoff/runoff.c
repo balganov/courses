c697155562a2a5391c8cec2e960733b430477d4b
balganov <160471135+balganov@users.noreply.github.com>
2024-04-07 06:24:25 +0000

    Sun, Apr 7, 2024, 11:24 AM +05:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -151,9 +151,9 @@ void tabulate(void)
     {
         for (int i = 0; i < candidate_count; i++)
         {
-            if (candidates[i].eliminated == false && preferences[k][i] == )
+            if (candidates[i].eliminated == false && preferences[k][0] == i)
             {
-
+                candidates[i].votes += 1;
             }
 
         }
