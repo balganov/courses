eef9c87c96ecf0bd1bfbde0fd680075fe2a86b95
balganov <160471135+balganov@users.noreply.github.com>
2024-04-07 07:44:38 +0000

    Sun, Apr 7, 2024, 12:44 PM +05:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -171,7 +171,16 @@ void tabulate(void)
 bool print_winner(void)
 {
     // TODO
-    return false;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes > voter_count/2)
+        {
+            printf("%s\n",candidates[i].name);
+            return true;
+        }
+        else
+            return false;
+    }
 }
 
 // Return the minimum number of votes any remaining candidate has
