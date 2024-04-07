7ed2993cb8f86a23b94026abd2e2881cd843d737
balganov <160471135+balganov@users.noreply.github.com>
2024-04-07 05:44:41 +0000

    Sun, Apr 7, 2024, 10:44 AM +05:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -130,7 +130,11 @@ bool vote(int voter, int rank, string name)
     bool valid = false;
     for (int i = 0; i < candidate_count; i++)
     {
-        
+        if (strcmp(candidates[i].name, name) == 0)
+        {
+            candidates[i].votes += 1;
+            found = true;
+        }
     }
     return false;
 }
