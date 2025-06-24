814bac9a5fc01c14fb7638295099ef8e02cea1b9
balganov <160471135+balganov@users.noreply.github.com>
2025-06-24 12:08:09 +0000

    Tue, Jun 24, 2025, 5:08 PM +05:00

--- cs50p/plates/plates.py
+++ cs50p/plates/plates.py
@@ -7,7 +7,7 @@ def main():
 
 
 def is_valid(s):
-    if s[0:2].isalpha() and len(s) <= 6 and s.endswith():
+    if s[0:2].isalpha() and len(s) <= 6 and not s.endswith(0,1,2,3,4,5,6,7,8,9):
         return True
     else:
         return False
