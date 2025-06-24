2152da4536d0d14b430fb05f8a8d1719fa1722b5
balganov <160471135+balganov@users.noreply.github.com>
2025-06-24 12:17:15 +0000

    Tue, Jun 24, 2025, 5:17 PM +05:00

--- cs50p/plates/plates.py
+++ cs50p/plates/plates.py
@@ -8,18 +8,11 @@ def main():
 
 def is_valid(s):
     if s[0:2].isalpha() and len(s) <= 6 and s[-1].isdigit() and s.isalnum():
-        return True
+        for c in s:
+            if c.isdigit() and c != 0:
+                return True
     else:
         return False
-
-    for c in s:
-        if c.isdigit():
-            counter = counter + 1
-            if c = 0 and counter = 1
-                return False
-            else:
-                return True
-
 
 
 main()
