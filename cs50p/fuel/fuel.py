67102f5ffd336d5cc25dc219f310fa3c25e18865
balganov <160471135+balganov@users.noreply.github.com>
2025-06-27 11:57:51 +0000

    Fri, Jun 27, 2025, 4:57 PM +05:00

--- cs50p/fuel/fuel.py
+++ cs50p/fuel/fuel.py
@@ -8,7 +8,12 @@ def main():
 def how_much(left):
     l = left.split("/")
     result = int(int(l[0])/int(l[1])*100)
-    
-    return str(result) + "%"
+
+    if result <= 1:
+        return "E"
+    elif result >= 99:
+        return "F"
+    else:
+        return str(result) + "%"
 
 main()
