dafd580a3364eb1d7eb97f4a7ac648d66345c132
balganov <160471135+balganov@users.noreply.github.com>
2025-06-27 11:50:03 +0000

    Fri, Jun 27, 2025, 4:50 PM +05:00

--- cs50p/fuel/fuel.py
+++ cs50p/fuel/fuel.py
@@ -1,11 +1,13 @@
 
 
-def main:
-f = input("Fraction: ")
+def main():
+    fuel = input("Fraction: ")
+    print(how_much(fuel))
 
 
-
-def how_much(f):
-
+def how_much(left):
+    l = left.split("/")
+    result = int(l[0])/int(l[1])*100
+    return result + "%"
 
 main()
