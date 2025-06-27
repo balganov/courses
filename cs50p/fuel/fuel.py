76ffaadd220e832239be35de1743d63b0a694993
balganov <160471135+balganov@users.noreply.github.com>
2025-06-27 12:24:30 +0000

    Fri, Jun 27, 2025, 5:24 PM +05:00

--- cs50p/fuel/fuel.py
+++ cs50p/fuel/fuel.py
@@ -3,20 +3,20 @@
 def main():
     print(get_fuel())
 
-def get_fuel(left):
-    l = left.split("/")
-    result = int(round(int(l[0])/int(l[1])*100))
-
-    while True
+def get_fuel():
+    while True:
         try:
-            fuel = input("Fraction: ")
-            
+            l = input("Fraction: ").split("/")
+            result = int(round(int(l[0])/int(l[1])*100))
+
             if result <= 1:
-                return "E"
+                print("E")
+            elif result > 100:
+                raise Exception()
             elif result >= 99:
-                return "F"
+                print("F")
             else:
-                return str(result) + "%"
+                print(str(result) + "%")
         except (ValueError, ZeroDivisionError):
             pass
 
