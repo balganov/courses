a96bfa5cf46ccea79f99594417e0cfccf7565846
balganov <160471135+balganov@users.noreply.github.com>
2025-11-15 10:35:33 +0000

    Sat, Nov 15, 2025, 3:35 PM +05:00

--- players/6.sql
+++ players/6.sql
@@ -1,4 +1,4 @@
 SELECT "first_name", "last_name", "debut"
 FROM "players"
 WHERE "birth_city" = 'Pittsburgh' AND "birth_state" = 'PA'
-ORDER BY "last_name", "first_name", "debut" DESC;
+ORDER BY "debut" DESC, "first_name", "last_name";
