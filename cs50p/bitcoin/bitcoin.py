bf4cb86acdd45aebe315dadb1b102e6536867642
balganov <160471135+balganov@users.noreply.github.com>
2025-11-16 05:02:37 +0000

    Sun, Nov 16, 2025, 10:02 AM +05:00

--- cs50p/bitcoin/bitcoin.py
+++ cs50p/bitcoin/bitcoin.py
@@ -1,12 +1,17 @@
 import sys
 import requests
 
+
+def is_float(value):
+    if float(value): return True else: return False
+
+
 def main():
 
     if len(sys.argv) == 1:
         print("Missing command-line argument\n")
         sys.exit()
-    elif float(sys.argv[1]) is False:
+    elif float(sys.argv[1]):
         print("Command-line argument is not a number\n")
         sys.exit()
     else:
