81ce5d3186ee660f1887377a770371baaf83a8e6
balganov <160471135+balganov@users.noreply.github.com>
2025-11-16 10:56:18 +0000

    Sun, Nov 16, 2025, 3:56 PM +05:00

---
+++ cs50p/test_twttr/twttr.py
@@ -0,0 +1,16 @@
+
+def main():
+    result = shorten(input("Input : "))
+    print(f"Output: {result}")
+
+def shorten(word):
+    v = ("A", "E", "I", "O", "U")
+    new_word = ""
+    for i in word:
+        if not i.upper() in v:
+            new_word += i
+
+    return new_word
+
+if __name__ == "__main__":
+    main()
