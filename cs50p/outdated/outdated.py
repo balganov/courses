29023cc686bc18e8dd2ab5f3e99b8d4a36606421
balganov <160471135+balganov@users.noreply.github.com>
2025-11-17 09:55:06 +0000

    Mon, Nov 17, 2025, 2:55 PM +05:00

--- cs50p/outdated/outdated.py
+++ cs50p/outdated/outdated.py
@@ -26,8 +26,11 @@ def main():
             else:
                 prep = date.replace(",","").split()
                 if prep[0].capitalize() in months:
-                    print(f"{prep[1]:02}-{months.index(prep[0].capitalize())+1:02}-{prep[2]}")
-                    return
+                    prep[0] = months.index(prep[0].capitalize())+1
+                    int_list = [int(s) for s in prep]
+                    if 1 <= int_list[0] <= 12 and 1 <= int_list[1] <= 31:
+                        print(f"{int_list[1]:02d}-{int_list[0]:02d}-{int_list[2]}")
+                        return
                 else:
                     raise ValueError
         except:
