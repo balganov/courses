ff498dd6eb7e7ff11c95c988360ca0ad490cf11d
balganov <160471135+balganov@users.noreply.github.com>
2025-11-17 09:31:03 +0000

    Mon, Nov 17, 2025, 2:31 PM +05:00

--- cs50p/outdated/outdated.py
+++ cs50p/outdated/outdated.py
@@ -15,10 +15,17 @@ def main():
     while True:
         try:
             date = input("Date: ")
-            prep = date.replace(",","").split()
-            if prep[0].capitalize() in months:
-                print(f"{prep[1]:02}/{months.index(prep[0].capitalize())}/{prep[2]}")
-                return
+            if date[0].isnumeric():
+                prep = date.split("/")
+                if prep[0].capitalize() in months:
+                    print(f"{prep[1]:02}/{months.index(prep[0].capitalize())}/{prep[2]}")
+                    return
+                else:
+                    raise ValueError
+            else:
+                prep = date.replace(",","").split()
+
+
             else:
                 print(prep)
                 return
