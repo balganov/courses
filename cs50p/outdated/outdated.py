ceb9522efcd47d755afceaf1d7e7dd5ce522898a
balganov <160471135+balganov@users.noreply.github.com>
2025-11-17 10:11:30 +0000

    Mon, Nov 17, 2025, 3:11 PM +05:00

--- cs50p/outdated/outdated.py
+++ cs50p/outdated/outdated.py
@@ -27,6 +27,7 @@ def main():
                 if ',' not in date and prep[0].capitalize() in months:
                     prep = date.replace(",","").split()
                     prep[0] = months.index(prep[0].capitalize())+1
+                    print(prep)
                     int_list = [int(s) for s in prep]
                     if 1 <= int_list[0] <= 12 and 1 <= int_list[1] <= 31 and 1000 <= int_list[2] <= 9999:
                         print(f"{int_list[2]:04d}-{int_list[0]:02d}-{int_list[1]:02d}")
