6fce4cdf96c0305844615f2601deb81a29b35967
balganov <160471135+balganov@users.noreply.github.com>
2025-11-17 10:18:22 +0000

    Mon, Nov 17, 2025, 3:18 PM +05:00

--- cs50p/outdated/outdated.py
+++ cs50p/outdated/outdated.py
@@ -24,14 +24,16 @@ def main():
                 else:
                     raise ValueError
             else:
-                if ',' not in date:
-                    prep = date.replace(",","").split()
+                if ',' in date:
                     if prep[0].capitalize() in months:
+                        prep = date.replace(",","").split()
                         prep[0] = months.index(prep[0].capitalize())+1
                         int_list = [int(s) for s in prep]
                         if 1 <= int_list[0] <= 12 and 1 <= int_list[1] <= 31 and 1000 <= int_list[2] <= 9999:
                             print(f"{int_list[2]:04d}-{int_list[0]:02d}-{int_list[1]:02d}")
                             return
+                    else:
+                        raise ValueError
                 else:
                     raise ValueError
         except:
