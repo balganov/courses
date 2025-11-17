2417eb2fb3b68fd628690f78b7d06732bb066c89
balganov <160471135+balganov@users.noreply.github.com>
2025-11-17 08:38:38 +0000

    Mon, Nov 17, 2025, 1:38 PM +05:00

---
+++ cs50p/outdated/outdated.py
@@ -0,0 +1,17 @@
+def main():
+    months = ["January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"]
+
+    date = input("Date: ")
+    
+main()
