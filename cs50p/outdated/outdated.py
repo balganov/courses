e60fe2c13966cd38a0f80ef10f6d7e0a97e2fbbe
balganov <160471135+balganov@users.noreply.github.com>
2025-11-17 10:19:35 +0000

    Mon, Nov 17, 2025, 3:19 PM +05:00

--- cs50p/outdated/outdated.py
+++ cs50p/outdated/outdated.py
@@ -25,8 +25,8 @@ def main():
                     raise ValueError
             else:
                 if ',' in date:
+                    prep = date.replace(",","").split()
                     if prep[0].capitalize() in months:
-                        prep = date.replace(",","").split()
                         prep[0] = months.index(prep[0].capitalize())+1
                         int_list = [int(s) for s in prep]
                         if 1 <= int_list[0] <= 12 and 1 <= int_list[1] <= 31 and 1000 <= int_list[2] <= 9999:
