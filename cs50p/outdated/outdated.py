5a3b1a977f5768c19d59dad80993b1f06d307c6e
balganov <160471135+balganov@users.noreply.github.com>
2025-11-17 10:10:11 +0000

    Mon, Nov 17, 2025, 3:10 PM +05:00

--- cs50p/outdated/outdated.py
+++ cs50p/outdated/outdated.py
@@ -14,7 +14,7 @@ def main():
 
     while True:
         try:
-            date = input("Date: ")
+            date = input("Date: ").strip()
             if '/' in date and date[0].isdigit():
                 prep = date.split("/")
                 int_list = [int(s) for s in prep]
@@ -24,8 +24,8 @@ def main():
                 else:
                     raise ValueError
             else:
-                prep = date.replace(",","").split()
                 if ',' not in date and prep[0].capitalize() in months:
+                    prep = date.replace(",","").split()
                     prep[0] = months.index(prep[0].capitalize())+1
                     int_list = [int(s) for s in prep]
                     if 1 <= int_list[0] <= 12 and 1 <= int_list[1] <= 31 and 1000 <= int_list[2] <= 9999:
