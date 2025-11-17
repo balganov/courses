e18727d80e1ed3dcf666e52eb14a445935ed7ecd
balganov <160471135+balganov@users.noreply.github.com>
2025-11-17 09:34:27 +0000

    Mon, Nov 17, 2025, 2:34 PM +05:00

--- cs50p/outdated/outdated.py
+++ cs50p/outdated/outdated.py
@@ -18,14 +18,14 @@ def main():
             if date[0].isnumeric():
                 prep = date.split("/")
                 if prep[0] <= 12 or prep[1] <= 31:
-                    print(f"{prep[1]:02}/{prep[0]:02}/{prep[2]}")
+                    print(f"{prep[1]:02}-{prep[0]:02}-{prep[2]}")
                     return
                 else:
                     raise ValueError
             else:
                 prep = date.replace(",","").split()
                 if prep[0].capitalize() in months:
-                    print(f"{prep[1]:02}/{months.index(prep[0].capitalize())}/{prep[2]}")
+                    print(f"{prep[1]:02}-{months.index(prep[0].capitalize())}-{prep[2]}")
                     return
                 else:
                     raise ValueError
