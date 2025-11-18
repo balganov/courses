dfc1fc0bd335035f56040756387999492d53801a
balganov <160471135+balganov@users.noreply.github.com>
2025-11-18 13:10:15 +0000

    Tue, Nov 18, 2025, 6:10 PM +05:00

--- dese/11.sql
+++ dese/11.sql
@@ -2,5 +2,4 @@ SELECT "s"."name", "e"."per_pupil_expenditure", "gr"."graduated"
 FROM "schools" AS "s"
 JOIN "expenditures" AS "e" ON "s"."district_id" = "e"."district_id"
 JOIN "graduation_rates" AS "gr" ON "s"."id" = "gr"."school_id"
-ORDER BY "e"."per_pupil_expenditure" DESC, "s"."name"
-LIMIT 10;
+ORDER BY "e"."per_pupil_expenditure" DESC, "s"."name";
