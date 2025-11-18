b95fcb9794e19f1cc53a16931eeb502e110a687e
balganov <160471135+balganov@users.noreply.github.com>
2025-11-18 13:02:11 +0000

    Tue, Nov 18, 2025, 6:02 PM +05:00

--- dese/13.sql
+++ dese/13.sql
@@ -1,4 +1,4 @@
-SELECT "d"."name", "se"."needs_improvement"
+SELECT "d"."state", "se"."needs_improvement"
 FROM "districts" as "d"
 JOIN "staff_evaluations" as "se" ON "d"."id" = "se"."district_id"
 ORDER BY "se"."needs_improvement" DESC
