9212f679eb4cb5d67464e7b85305bb8b1ebec3d6
balganov <160471135+balganov@users.noreply.github.com>
2025-11-18 13:04:30 +0000

    Tue, Nov 18, 2025, 6:04 PM +05:00

--- dese/13.sql
+++ dese/13.sql
@@ -1,5 +1,6 @@
-SELECT "d"."state", "se"."needs_improvement"
+SELECT "d"."state", AVG("se"."needs_improvement") "Average need imrpovement"
 FROM "districts" as "d"
 JOIN "staff_evaluations" as "se" ON "d"."id" = "se"."district_id"
-ORDER BY "se"."needs_improvement" DESC
+GROUP BY "d"."state"
+ORDER BY "Average need imrpovement" DESC
 LIMIT 5;
