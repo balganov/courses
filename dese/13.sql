565cede131438f6d40badba42dc97840d5447e2d
balganov <160471135+balganov@users.noreply.github.com>
2025-11-18 13:01:33 +0000

    Tue, Nov 18, 2025, 6:01 PM +05:00

---
+++ dese/13.sql
@@ -0,0 +1,5 @@
+SELECT "d"."name", "se"."needs_improvement"
+FROM "districts" as "d"
+JOIN "staff_evaluations" as "se" ON "d"."id" = "se"."district_id"
+ORDER BY "se"."needs_improvement" DESC
+LIMIT 5;
