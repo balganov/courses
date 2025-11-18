43972df1ea378f99ee800f8664bee37b5327c922
balganov <160471135+balganov@users.noreply.github.com>
2025-11-18 12:04:56 +0000

    Tue, Nov 18, 2025, 5:04 PM +05:00

--- dese/4.sql
+++ dese/4.sql
@@ -1,8 +1,8 @@
 --SELECT COUNT(*) FROM (
-SELECT "d"."city", COUNT("s"."name") AS 'Number of Schools'
+SELECT "d"."city", COUNT("s"."name") AS "Number of Schools"
 FROM "schools" AS "s"
 JOIN "districts" AS "d" ON "s"."district_id" = "d"."id"
 GROUP BY "d"."city"
-ORDER BY 'Number of Schools'
+ORDER BY "Number of Schools" DESC, "d"."city"
 LIMIT 10
 --);
