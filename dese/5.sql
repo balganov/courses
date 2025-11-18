e7cbca49f49cbfb767088d6eaccb779ef6c85585
balganov <160471135+balganov@users.noreply.github.com>
2025-11-18 12:08:11 +0000

    Tue, Nov 18, 2025, 5:08 PM +05:00

--- dese/5.sql
+++ dese/5.sql
@@ -3,5 +3,6 @@ FROM "schools" AS "s"
 JOIN "districts" AS "d" ON "s"."district_id" = "d"."id"
 WHERE "s"."type" = 'Public School'
 GROUP BY "d"."city"
+HAVING "Number of Schools" <= 3
 ORDER BY "Number of Schools" DESC, "d"."city"
 LIMIT 10
