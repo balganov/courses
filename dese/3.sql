a09360ea73f73fa977391cdc9a3055f3aff30b4c
balganov <160471135+balganov@users.noreply.github.com>
2025-11-18 11:58:22 +0000

    Tue, Nov 18, 2025, 4:58 PM +05:00

--- dese/3.sql
+++ dese/3.sql
@@ -1,6 +1,4 @@
-SELECT COUNT(*) FROM (
-SELECT "d"."name", AVG("per_pupil_expenditure") AS 'Average District Per-Pupil Expenditure'
-FROM "districts" AS "d"
-JOIN "expenditures" AS "e" ON "d"."id" = "e"."district_id"
-GROUP BY "d"."name"
-);
+--SELECT COUNT(*) FROM (
+SELECT AVG("per_pupil_expenditure") AS 'Average District Per-Pupil Expenditure'
+FROM "expenditures";
+--);
---
+++ dese/4.sql
@@ -0,0 +1,4 @@
+--SELECT COUNT(*) FROM (
+SELECT AVG("per_pupil_expenditure") AS 'Average District Per-Pupil Expenditure'
+FROM "expenditures";
+--);
