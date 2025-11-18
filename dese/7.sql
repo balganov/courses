57ed80cecceece0d26feca12e5266e904885b033
balganov <160471135+balganov@users.noreply.github.com>
2025-11-18 12:21:24 +0000

    Tue, Nov 18, 2025, 5:21 PM +05:00

--- dese/7.sql
+++ dese/7.sql
@@ -1,4 +1,4 @@
-SELECT "s"."name", "d"."name"
+SELECT "s"."name"
 FROM "schools" AS "s"
 JOIN "districts" AS "d" ON "s"."district_id" = "d"."id"
-WHERE "d"."name" = 'Cambridge'
+WHERE "d"."name" = 'Cambridge';
