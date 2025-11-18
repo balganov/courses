6285f399414d8fb7708f23b11686f5ffef1b7638
balganov <160471135+balganov@users.noreply.github.com>
2025-11-18 13:13:16 +0000

    Tue, Nov 18, 2025, 6:13 PM +05:00

--- dese/12.sql
+++ dese/12.sql
@@ -4,4 +4,5 @@ JOIN "expenditures" AS "e" ON "d"."id" = "e"."district_id"
 JOIN "staff_evaluations" AS "se" ON "d"."id" = "se"."district_id"
 WHERE "e"."per_pupil_expenditure" > (SELECT AVG("per_pupil_expenditure") FROM "expenditures")
 AND "se"."exemplary" > (SELECT AVG("exemplary") FROM "staff_evaluations")
+AND "d"."type" = 'Public School District'
 ORDER BY "se"."exemplary" DESC, "e"."per_pupil_expenditure" DESC;
