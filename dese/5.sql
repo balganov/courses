15d0b8afea94ef8693407e941d90cdec06981c6a
balganov <160471135+balganov@users.noreply.github.com>
2025-11-18 12:14:49 +0000

    Tue, Nov 18, 2025, 5:14 PM +05:00

--- dese/5.sql
+++ dese/5.sql
@@ -1,8 +1,8 @@
-SELECT COUNT(*) FROM (
+--SELECT COUNT(*) FROM (
 SELECT "city", COUNT("name") AS "Number of Schools"
 FROM "schools"
 WHERE "type" = 'Public School'
 GROUP BY "city"
 HAVING "Number of Schools" <= 3
-ORDER BY "Number of Schools" DESC, "city"
-);
+ORDER BY "Number of Schools" DESC, "city";
+--);
