beeb6c816fa5df23bc5bddf6a7074c953ba7f817
balganov <160471135+balganov@users.noreply.github.com>
2025-11-18 12:05:48 +0000

    Tue, Nov 18, 2025, 5:05 PM +05:00

---
+++ dese/5.sql
@@ -0,0 +1,6 @@
+SELECT "d"."city", COUNT("s"."name") AS "Number of Schools"
+FROM "schools" AS "s"
+JOIN "districts" AS "d" ON "s"."district_id" = "d"."id"
+GROUP BY "d"."city"
+ORDER BY "Number of Schools" DESC, "d"."city"
+LIMIT 10
