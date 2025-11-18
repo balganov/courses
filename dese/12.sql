90d6d3e171031140c3f7ef64a17b3c8e4b68e186
balganov <160471135+balganov@users.noreply.github.com>
2025-11-18 12:59:19 +0000

    Tue, Nov 18, 2025, 5:59 PM +05:00

---
+++ dese/12.sql
@@ -0,0 +1,7 @@
+SELECT "d"."name", "e"."per_pupil_expenditure", "se"."exemplary"
+FROM "districts" AS "d"
+JOIN "expenditures" AS "e" ON "d"."id" = "e"."district_id"
+JOIN "staff_evaluations" AS "se" ON "d"."id" = "se"."district_id"
+WHERE "e"."per_pupil_expenditure" > (SELECT AVG("per_pupil_expenditure") FROM "expenditures")
+AND "se"."exemplary" > (SELECT AVG("exemplary") FROM "staff_evaluations");
+ORDER BY "se"."exemplary" DESC, "e"."per_pupil_expenditure" DESC
