88104007c511f5c74381c8378a32819117d54741
balganov <160471135+balganov@users.noreply.github.com>
2025-11-23 05:56:18 +0000

    Sun, Nov 23, 2025, 10:56 AM +05:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -5,8 +5,7 @@ def main():
     print(validate(input("IPv4 Address: ")))
 
 def validate(ip):
-    if match := re.search(r"^(([0-9]|[0-9]{2}|1[0-9]{2}|2[0-5]{2})\.){3}([0-9]|[0-9]{2}|1[0-9]{2}|2[0-5]{2})$",ip):
-        print(match.group(0),match.group(1),match.group(2),match.group(3))
+    if re.search(r"^(([0-9]|[0-9]{2}|1[0-9]{2}|2[0-5]{2})\.){3}([0-9]|[0-9]{2}|1[0-9]{2}|2[0-5]{2})$",ip):
         return True
     else:
         return False
