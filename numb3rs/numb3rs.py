09322d562eab282626e77d126d2695e9bd8f1149
balganov <160471135+balganov@users.noreply.github.com>
2025-11-23 05:37:58 +0000

    Sun, Nov 23, 2025, 10:37 AM +05:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -7,7 +7,7 @@ def main():
 
 
 def validate(ip):
-    match = re.search(r"^[0-9][0-9]?$",ip)
+    match = re.search(r"^[0-9]|[0-9]{2}$",ip)
     if match:
         print("Valid.")
         print(match.group())
