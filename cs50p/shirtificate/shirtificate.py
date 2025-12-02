6891f0d5785114ba2ace79065faaf25c73dc743b
balganov <160471135+balganov@users.noreply.github.com>
2025-12-02 05:55:03 +0000

    Tue, Dec 2, 2025, 10:55 AM +05:00

--- cs50p/shirtificate/shirtificate.py
+++ cs50p/shirtificate/shirtificate.py
@@ -4,7 +4,7 @@ def main():
     pdf = FPDF(orientation="P", format="A4")
     pdf.add_page()
     pdf.set_font("helvetica", style="B", size=40)
-    pdf.cell("CS50 Shirtificate", center=True)
+    pdf.cell(40, 10, "CS50 Shirtificate", align="C")
     pdf.output("shirtificate.pdf")
 
 if __name__ == "__main__":
