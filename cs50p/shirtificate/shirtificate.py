e42c2e8f1eb4203adfaa91180ac9ac2d11453cbf
balganov <160471135+balganov@users.noreply.github.com>
2025-12-02 06:23:31 +0000

    Tue, Dec 2, 2025, 11:23 AM +05:00

--- cs50p/shirtificate/shirtificate.py
+++ cs50p/shirtificate/shirtificate.py
@@ -1,15 +1,16 @@
 from fpdf import FPDF
 
 def main():
+    name = input("Name: ")
     pdf = FPDF(orientation="P", format="A4")
     pdf.add_page()
     pdf.set_font("helvetica", style="B", size=40)
     pdf.cell(0, 50, "CS50 Shirtificate", align="C")
     pdf.image("shirtificate.png", w=200, x=5, y=70)
     pdf.set_text_color(r=255,g=255,b=255)
-    pdf.set_font_size(30)
+    pdf.set_font_size(25)
     pdf.set_xy(0,0)
-    pdf.cell(0, 250, "Nursultan took CS50", align="C",center=True)
+    pdf.cell(0, 270, f"{name} took CS50", align="C",center=True)
     pdf.output("shirtificate.pdf")
 
 if __name__ == "__main__":
