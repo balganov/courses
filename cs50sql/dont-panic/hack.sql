3c0b83d71261149922e33669faf48e6a3079d75f
balganov <160471135+balganov@users.noreply.github.com>
2025-12-03 03:54:13 +0000

    Wed, Dec 3, 2025, 8:54 AM +05:00

--- cs50sql/dont-panic/hack.sql
+++ cs50sql/dont-panic/hack.sql
@@ -3,5 +3,5 @@ UPDATE "users" SET "password" = 'oops!' WHERE "username" = 'admin';
 DELETE FROM "user_logs" WHERE "new_password" = 'oops!';
 
 
-INSERT INTO "user_logs" ("type","old_username","new_username","old_password","new_password",)
-
+INSERT INTO "user_logs" ("type","old_username","new_username","old_password","new_password")
+SELECT 'update', 'admin', 'admin', (SELECT "password" FROM "users" WHERE "username" = 'admin'),(SELECT "password" FROM "users" WHERE "username" = 'emily33')
