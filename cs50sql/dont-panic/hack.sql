d6936cee72ddedc7663ea33f8095c1cf37e5e507
balganov <160471135+balganov@users.noreply.github.com>
2025-12-03 03:49:41 +0000

    Wed, Dec 3, 2025, 8:49 AM +05:00

--- cs50sql/dont-panic/hack.sql
+++ cs50sql/dont-panic/hack.sql
@@ -1,3 +1,7 @@
 UPDATE "users" SET "password" = 'oops!' WHERE "username" = 'admin';
 
+DELETE FROM "user_logs" WHERE "new_password" = 'oops!';
+
+
+INSERT INTO "user_logs" ("type","old_username","new_username","old_password","new_password",)
 
