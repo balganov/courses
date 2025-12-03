b1865ded944a601370f9f9498e5ce90f34e403fd
balganov <160471135+balganov@users.noreply.github.com>
2025-12-03 04:04:40 +0000

    Wed, Dec 3, 2025, 9:04 AM +05:00

--- cs50sql/meteorites/import.sql
+++ cs50sql/meteorites/import.sql
@@ -1,3 +1,11 @@
 CREATE TABLE "meteorites" (
-
+    "id" INTEGER,
+    "name" TEXT,
+    "class" TEXT,
+    "mass" NUMERIC,
+    "discovery" TEXT,
+    "year" DATETIME,
+    "lat" NUMERIC,
+    "long" NUMERIC,
+    PRIMARY KEY("id")
 )
