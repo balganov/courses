db61eb94dca6b57c56fc5cd501d495d0f84b6df9
balganov <160471135+balganov@users.noreply.github.com>
2025-12-05 14:24:38 +0000

    Fri, Dec 5, 2025, 7:24 PM +05:00

--- cs50p/final_project/project.py
+++ cs50p/final_project/project.py
@@ -6,8 +6,8 @@ def main():
 
     # First we fetch dictionaries from corresponding endpoints and write them to local json flies
     #fetch_dictionaries()
-    #print(f"Please select the roles you are interested in:\n\n{get_roles()}")
-    print(f"Please select your preferred work locations:\n\n{get_areas()}")
+    print(f"Please specify the job roles you are seeking:\n{get_roles()}")
+    print(f"Please select your preferred work locations:\n{get_areas()}")
     print("done")
 
 def fetch_dictionaries():
