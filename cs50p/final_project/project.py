cf988c4c0d355e75c68a2f74b6e5ef6c545c0aa0
balganov <160471135+balganov@users.noreply.github.com>
2025-12-05 12:29:37 +0000

    Fri, Dec 5, 2025, 5:29 PM +05:00

--- cs50p/final_project/project.py
+++ cs50p/final_project/project.py
@@ -4,7 +4,7 @@ import requests
 def main():
 
 
-    job_roles = r.json()
+
 
     for i, c in enumerate(job_roles["categories"]):
         if c["id"] == "11":
@@ -16,6 +16,8 @@ def fetch_dictionaries():
         roles = requests.get('https://api.hh.ru/professional_roles')
         areas = requests.get('https://api.hh.ru/areas')
 
+        job_roles = roles.json()
+        countries = areas.json()
 
     except requests.RequestException as e:
         print(e)
