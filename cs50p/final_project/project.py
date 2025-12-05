6f7efd77ec359cff1b57ca9dde83a6df3fd8a9aa
balganov <160471135+balganov@users.noreply.github.com>
2025-12-05 12:48:36 +0000

    Fri, Dec 5, 2025, 5:48 PM +05:00

--- cs50p/final_project/project.py
+++ cs50p/final_project/project.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def main():
 
@@ -11,11 +12,11 @@ def fetch_dictionaries():
         roles = requests.get('https://api.hh.ru/professional_roles')
         areas = requests.get('https://api.hh.ru/areas')
 
-        with open("job_roles","w") as file:
-            file.write(str(roles.json()))
+        with open("job_roles.json","w") as f:
+            json.dump(roles.json(),f,indent=4)
 
-        with open("areas", "w") as file:
-            file.write(str(areas.json()))
+        with open("areas.json", "w") as f:
+            json.dump(areas.json(),f,indent=4)
 
 
     except requests.RequestException as e:
