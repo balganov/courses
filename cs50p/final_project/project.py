0cac5ce30d52b95877acd8804922bcc2bde9d60f
balganov <160471135+balganov@users.noreply.github.com>
2025-12-05 12:54:20 +0000

    Fri, Dec 5, 2025, 5:54 PM +05:00

--- cs50p/final_project/project.py
+++ cs50p/final_project/project.py
@@ -4,7 +4,7 @@ import json
 def main():
 
     # First we fetch dictionaries from corresponding endpoints and write them to local json flies
-    # fetch_dictionaries()
+    fetch_dictionaries()
 
     print("done")
 
@@ -13,11 +13,11 @@ def fetch_dictionaries():
         roles = requests.get('https://api.hh.ru/professional_roles')
         areas = requests.get('https://api.hh.ru/areas')
 
-        with open("job_roles.json","w") as f:
-            json.dump(roles.json(),f,indent=4)
+        with open("job_roles.json","w", encoding="utf-8") as f:
+            json.dump(roles.json(),f,indent=4, ensure_ascii=False)
 
-        with open("areas.json", "w") as f:
-            json.dump(areas.json(),f,indent=4)
+        with open("areas.json", "w", encoding="utf-8") as f:
+            json.dump(areas.json(),f,indent=4, ensure_ascii=False)
 
 
     except requests.RequestException as e:
