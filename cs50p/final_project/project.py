b3a587a2b9f90b21be47defcdbe94baa5172c268
balganov <160471135+balganov@users.noreply.github.com>
2025-12-05 14:17:29 +0000

    Fri, Dec 5, 2025, 7:17 PM +05:00

--- cs50p/final_project/project.py
+++ cs50p/final_project/project.py
@@ -34,7 +34,7 @@ def get_areas():
     with open("areas.json", "r", encoding="utf-8") as f:
         areas = json.load(f)
 
-    return GoogleTranslator(source="ru", target="en").translate('\n'.join(f"{i+1} {e['name'] for i, e in enumerate(areas) if e["name"] != 'Другие регионы'}"))
+    return GoogleTranslator(source="ru", target="en").translate('\n'.join(f"{i+1} {e['name']}" for i, e in enumerate(areas) if e["name"] != "Другие регионы"))
 
 if __name__ == "__main__":
     main()
