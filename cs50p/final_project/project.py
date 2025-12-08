3c5845a619c9812ad31b15a272157a04caf0d1a5
balganov <160471135+balganov@users.noreply.github.com>
2025-12-08 04:06:34 +0000

    Mon, Dec 8, 2025, 9:06 AM +05:00

--- cs50p/final_project/project.py
+++ cs50p/final_project/project.py
@@ -1,19 +1,22 @@
 import requests
 import json
-from deep_translator import GoogleTranslator
 
 def main():
 
     # First we fetch dictionaries from corresponding endpoints and write them to local json flies
-    #fetch_dictionaries()
-    print(f"Please specify the job roles you are seeking:\n{get_roles()}")
-    print(f"Please select your preferred work locations:\n{get_areas()}")
+    # fetch_dictionaries()
+    # role_params = input(f"Please specify the job roles you are seeking:\n{get_roles()}\n").split(",")
+    # area_params = input(f"Please select your preferred work locations:\n{get_areas()}\n").split(",")
+
+    # fetch_vacancies(role_params, area_params)
+    analyze_vacancies()
+
     print("done")
 
 def fetch_dictionaries():
     try:
-        roles = requests.get('https://api.hh.ru/professional_roles')
-        areas = requests.get('https://api.hh.ru/areas')
+        roles = requests.get('https://api.hh.ru/professional_roles?locale=EN')
+        areas = requests.get('https://api.hh.ru/areas?locale=EN')
 
         with open("job_roles.json","w", encoding="utf-8") as f:
             json.dump(roles.json(),f,indent=4, ensure_ascii=False)
@@ -21,21 +24,51 @@ def fetch_dictionaries():
         with open("areas.json", "w", encoding="utf-8") as f:
             json.dump(areas.json(),f,indent=4, ensure_ascii=False)
 
+        print("fetching is done")
+
     except requests.RequestException as e:
         print(e)
+
+def fetch_vacancies(role_params, area_params):
+    try:
+        vacancy_params = {
+            "professional_role":role_params,
+            "area" : area_params,
+            "clusters":"true",
+            "per_page":"10",
+            "locale": "EN"
+            }
+
+        print(vacancy_params)
+
+        vacancies = requests.get('https://api.hh.ru/vacancies', params=vacancy_params)
+        print(vacancies.url)
+
+        with open("vacancies.json","w", encoding="utf-8") as f:
+             json.dump(vacancies.json(),f,indent=4, ensure_ascii=False)
+
+    except requests.RequestException as e:
+        print(e)
+
 
 def get_roles():
     with open("job_roles.json", "r", encoding="utf-8") as f:
         job_roles = json.load(f)
 
-    return GoogleTranslator(source="ru", target="en").translate('\n'.join(f"{i+1} {c['name']}" for i, c in enumerate(job_roles["categories"][7]["roles"])))
+    return '\n'.join(f"[{e['id']}]  {e['name']}" for e in job_roles["categories"][7]["roles"])
 
 def get_areas():
     with open("areas.json", "r", encoding="utf-8") as f:
         areas = json.load(f)
 
     areas = sorted(areas,key=lambda x: x['name'])
-    return GoogleTranslator(source="ru", target="en").translate('\n'.join(f"{i+1} {e['name']}" for i, e in enumerate(areas) if e['name'] != "Другие регионы"))
+    return '\n'.join(f"[{e['id']}]  {e['name']}" for e in areas if e['name'] != "Other regions")
+
+def analyze_vacancies():
+    with open("vacancies.json", "r", encoding="utf-8") as f:
+        vacancies = json.load(f)
+
+    print(f"Total: {vacancies['found']} Pages: {vacancies['pages']}")
 
 if __name__ == "__main__":
     main()
