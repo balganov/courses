14e27eb5140346f063791c80225f19d89bf00a98
balganov <160471135+balganov@users.noreply.github.com>
2025-12-08 06:58:20 +0000

    Mon, Dec 8, 2025, 11:58 AM +05:00

--- cs50p/final_project/project.py
+++ cs50p/final_project/project.py
@@ -96,8 +96,8 @@ def analyze_vacancies():
     for i in vacancies["clusters"][11]["items"]:
         print(f"{i['name']}: {i['count']}")
 
-    # for i in vacancies["items"][0]:
-    #     print(f"{i['url']}")
+    for i in vacancies["items"][0]:
+        print(f"{i['url']}")
 
 if __name__ == "__main__":
     main()
