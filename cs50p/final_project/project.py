df30adc6881e46e8fe4422c29bb2d127d84dd6d7
balganov <160471135+balganov@users.noreply.github.com>
2025-12-08 04:51:01 +0000

    Mon, Dec 8, 2025, 9:51 AM +05:00

--- cs50p/final_project/project.py
+++ cs50p/final_project/project.py
@@ -51,7 +51,7 @@ def fetch_vacancies(role_params, area_params):
         print(f"Total: {data['found']} Pages: {pages}")
 
         if pages > 1:
-            for p in range(1, pages-1):
+            for p in range(1, pages):
                 vacancy_params["clusters"] = "false"
                 vacancy_params["page"] = p
                 print(vacancy_params)
