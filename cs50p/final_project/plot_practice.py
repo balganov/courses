b6eedf1ef25067dd3f0e7946fcf42d0d0b9298af
balganov <160471135+balganov@users.noreply.github.com>
2025-12-12 09:31:19 +0000

    Fri, Dec 12, 2025, 2:31 PM +05:00

--- cs50p/final_project/plot_practice.py
+++ cs50p/final_project/plot_practice.py
@@ -3,7 +3,7 @@ import numpy as np
 
 data = {"city1":1,"city2":2,"city3":3,"city4":4}
 
-f, ax = plt.subplots(4,2)
+f, ax = plt.subplots(2,2)
 
 ax[0].pie(data.values(), labels=data.keys())
 ax[1].pie(data.values(), labels=data.keys())
