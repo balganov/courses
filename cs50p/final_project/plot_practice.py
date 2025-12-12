d897d18014f82f29b05a71478a79f439c8c3db9e
balganov <160471135+balganov@users.noreply.github.com>
2025-12-12 09:32:58 +0000

    Fri, Dec 12, 2025, 2:32 PM +05:00

--- cs50p/final_project/plot_practice.py
+++ cs50p/final_project/plot_practice.py
@@ -5,6 +5,7 @@ data = {"city1":1,"city2":2,"city3":3,"city4":4}
 
 f, ax = plt.subplots(2,2)
 
+ax[0].set_title('Pie Chart 1')
 ax[0].pie(data.values(), labels=data.keys())
 ax[1].pie(data.values(), labels=data.keys())
 ax[2].pie(data.values(), labels=data.keys())
