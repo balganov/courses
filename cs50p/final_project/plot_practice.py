b3e89112be8f62d2bf956e554c4b2fb75462902a
balganov <160471135+balganov@users.noreply.github.com>
2025-12-12 09:34:46 +0000

    Fri, Dec 12, 2025, 2:34 PM +05:00

--- cs50p/final_project/plot_practice.py
+++ cs50p/final_project/plot_practice.py
@@ -5,11 +5,14 @@ data = {"city1":1,"city2":2,"city3":3,"city4":4}
 
 f, ax = plt.subplots(2,2)
 
-ax[0].set_title('Pie Chart 1')
-ax[0].pie(data.values(), labels=data.keys())
-ax[1].pie(data.values(), labels=data.keys())
-ax[2].pie(data.values(), labels=data.keys())
-ax[3].pie(data.values(), labels=data.keys())
+ax[0,0].set_title('Pie Chart 1')
+ax[0,0].pie(data.values(), labels=data.keys())
+ax[0,1].set_title('Pie Chart 2')
+ax[0,1].pie(data.values(), labels=data.keys())
+ax[1,0].set_title('Pie Chart 3')
+ax[1,0].pie(data.values(), labels=data.keys())
+ax[1,1].set_title('Pie Chart 4')
+ax[1,1].pie(data.values(), labels=data.keys())
 
 
 plt.savefig("test.png")
