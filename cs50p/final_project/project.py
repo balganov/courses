2e0629db9592914fc17de466eaf6a656b09109d9
balganov <160471135+balganov@users.noreply.github.com>
2025-12-14 09:41:06 +0000

    Sun, Dec 14, 2025, 2:41 PM +05:00

--- cs50p/final_project/project.py
+++ cs50p/final_project/project.py
@@ -169,7 +169,7 @@ async def fetch_descriptions(vacancies):
     semaphore = asyncio.Semaphore(5)
 
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
+        "User-Agent": "JobAnalyzer/1.0 (sdf010121@gmail.com)",
         "Accept": "application/json",
         "Accept-Language": "en-US,en;q=0.9",
         "Referer": "https://hh.ru/"
