a629188e61153fb93f2250410a233c21deb56be0
balganov <160471135+balganov@users.noreply.github.com>
2025-12-14 03:51:03 +0000

    Sun, Dec 14, 2025, 8:51 AM +05:00

--- cs50p/final_project/check.py
+++ cs50p/final_project/check.py
@@ -1,4 +1,205 @@
 
-list = [1,2,3,4,"asd"]
-0
-print(all(isinstance(i,int) for i in list))
+list = [
+                {
+                    "id": "156",
+                    "name": "BI analyst, data analyst",
+                    "accept_incomplete_resumes": "false",
+                    "is_default": "false",
+                    "select_deprecated": "false",
+                    "search_deprecated": "false"
+                },
+                {
+                    "id": "160",
+                    "name": "DevOps engineer",
+                    "accept_incomplete_resumes": "false",
+                    "is_default": "false",
+                    "select_deprecated": "false",
+                    "search_deprecated": "false"
+                },
+                {
+                    "id": "10",
+                    "name": "Analyst",
+                    "accept_incomplete_resumes": "false",
+                    "is_default": "false",
+                    "select_deprecated": "false",
+                    "search_deprecated": "false"
+                },
+                {
+                    "id": "12",
+                    "name": "Art director, creative director",
+                    "accept_incomplete_resumes": "false",
+                    "is_default": "false",
+                    "select_deprecated": "false",
+                    "search_deprecated": "false"
+                },
+                {
+                    "id": "150",
+                    "name": "Business analyst",
+                    "accept_incomplete_resumes": "false",
+                    "is_default": "false",
+                    "select_deprecated": "false",
+                    "search_deprecated": "false"
+                },
+                {
+                    "id": "25",
+                    "name": "Game designer",
+                    "accept_incomplete_resumes": "false",
+                    "is_default": "false",
+                    "select_deprecated": "false",
+                    "search_deprecated": "false"
+                },
+                {
+                    "id": "165",
+                    "name": "Data scientist",
+                    "accept_incomplete_resumes": "false",
+                    "is_default": "false",
+                    "select_deprecated": "false",
+                    "search_deprecated": "false"
+                },
+                {
+                    "id": "34",
+                    "name": "Designer, artist",
+                    "accept_incomplete_resumes": "false",
+                    "is_default": "false",
+                    "select_deprecated": "false",
+                    "search_deprecated": "false"
+                },
+                {
+                    "id": "36",
+                    "name": "Chief information officer (CIO)",
+                    "accept_incomplete_resumes": "false",
+                    "is_default": "false",
+                    "select_deprecated": "false",
+                    "search_deprecated": "false"
+                },
+                {
+                    "id": "73",
+                    "name": "Product manager",
+                    "accept_incomplete_resumes": "false",
+                    "is_default": "false",
+                    "select_deprecated": "false",
+                    "search_deprecated": "false"
+                },
+                {
+                    "id": "155",
+                    "name": "Methodologist",
+                    "accept_incomplete_resumes": "false",
+                    "is_default": "false",
+                    "select_deprecated": "false",
+                    "search_deprecated": "false"
+                },
+                {
+                    "id": "96",
+                    "name": "Programmer, developer",
+                    "accept_incomplete_resumes": "false",
+                    "is_default": "false",
+                    "select_deprecated": "false",
+                    "search_deprecated": "false"
+                },
+                {
+                    "id": "164",
+                    "name": "Product analyst",
+                    "accept_incomplete_resumes": "false",
+                    "is_default": "false",
+                    "select_deprecated": "false",
+                    "search_deprecated": "false"
+                },
+                {
+                    "id": "104",
+                    "name": "Development team leader",
+                    "accept_incomplete_resumes": "false",
+                    "is_default": "false",
+                    "select_deprecated": "false",
+                    "search_deprecated": "false"
+                },
+                {
+                    "id": "157",
+                    "name": "Analytics department manager",
+                    "accept_incomplete_resumes": "false",
+                    "is_default": "false",
+                    "select_deprecated": "false",
+                    "search_deprecated": "false"
+                },
+                {
+                    "id": "107",
+                    "name": "Project manager",
+                    "accept_incomplete_resumes": "false",
+                    "is_default": "false",
+                    "select_deprecated": "false",
+                    "search_deprecated": "false"
+                },
+                {
+                    "id": "112",
+                    "name": "Network engineer",
+                    "accept_incomplete_resumes": "false",
+                    "is_default": "false",
+                    "select_deprecated": "false",
+                    "search_deprecated": "false"
+                },
+                {
+                    "id": "113",
+                    "name": "System administrator",
+                    "accept_incomplete_resumes": "false",
+                    "is_default": "false",
+                    "select_deprecated": "false",
+                    "search_deprecated": "false"
+                },
+                {
+                    "id": "148",
+                    "name": "Systems analyst",
+                    "accept_incomplete_resumes": "false",
+                    "is_default": "false",
+                    "select_deprecated": "false",
+                    "search_deprecated": "false"
+                },
+                {
+                    "id": "114",
+                    "name": "System engineer",
+                    "accept_incomplete_resumes": "false",
+                    "is_default": "false",
+                    "select_deprecated": "false",
+                    "search_deprecated": "false"
+                },
+                {
+                    "id": "116",
+                    "name": "Information security specialist",
+                    "accept_incomplete_resumes": "false",
+                    "is_default": "false",
+                    "select_deprecated": "false",
+                    "search_deprecated": "false"
+                },
+                {
+                    "id": "121",
+                    "name": "Technical support specialist",
+                    "accept_incomplete_resumes": True,
+                    "is_default": "false",
+                    "select_deprecated": "false",
+                    "search_deprecated": "false"
+                },
+                {
+                    "id": "124",
+                    "name": "Tester",
+                    "accept_incomplete_resumes": "false",
+                    "is_default": "false",
+                    "select_deprecated": "false",
+                    "search_deprecated": "false"
+                },
+                {
+                    "id": "125",
+                    "name": "Chief technical officer (CTO)",
+                    "accept_incomplete_resumes": "false",
+                    "is_default": "false",
+                    "select_deprecated": "false",
+                    "search_deprecated": "false"
+                },
+                {
+                    "id": "126",
+                    "name": "Technical writer",
+                    "accept_incomplete_resumes": "false",
+                    "is_default": "false",
+                    "select_deprecated": "false",
+                    "search_deprecated": "false"
+                }
+            ]
+
+print({'id':e['id'] for e in list})
