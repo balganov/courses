1a3ffffef03d5781e63e3cdffdcdd955cd82e50f
balganov <160471135+balganov@users.noreply.github.com>
2025-12-14 06:36:43 +0000

    Sun, Dec 14, 2025, 11:36 AM +05:00

--- cs50p/final_project/plot_practice.py
+++ cs50p/final_project/plot_practice.py
@@ -17,7 +17,7 @@ count_skills = {'SQL': 62, 'Python': 47, 'Power BI': 26, 'Анализ данн�
 r = 2
 c = 3
 n = 0
-f, ax = plt.subplots(r,c, figsize=(30,10))
+f, ax = plt.subplots(r,c, figsize=(15,13))
 colors = plt.get_cmap('viridis')(np.linspace(0.9, 0.4, len(data)))
 
 print(colors)
