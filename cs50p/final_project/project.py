36baabbcbff2d9101bfd338181bca744c32ef1e4
balganov <160471135+balganov@users.noreply.github.com>
2025-12-14 03:31:19 +0000

    Sun, Dec 14, 2025, 8:31 AM +05:00

--- cs50p/final_project/project.py
+++ cs50p/final_project/project.py
@@ -21,63 +21,64 @@ def validated_input(input):
 def main():
     global loading
     # First we fetch dictionaries from corresponding endpoints and write them to local json flies
-    fetch_dictionaries()
-    role_params = input(f"Please specify the job roles you are seeking:\n{get_roles()}\n").split(",")
-    area_params = input(f"Please select your preferred work locations:\n{get_areas()}\n").split(",")
+    #fetch_dictionaries()
+    get_roles()
+    #role_params = input(f"Please specify the job roles you are seeking:\n{get_roles()}\n").split(",")
+    #area_params = input(f"Please select your preferred work locations:\n{get_areas()}\n").split(",")
 
     #fetch_vacancies(role_params, area_params)
     #fetch_vacancies(['165','164','156'], ['40'])
     print("start")
 
-    #Creating a thread to animate the loading since our function takes some time to fetch data from multiple urls
-    loading_thread = threading.Thread(target=loading_animation)
-    loading_thread.start()
-    try:
-        data = get_summary()
-    finally:
-        loading = False
-        loading_thread.join()
+    # #Creating a thread to animate the loading since our function takes some time to fetch data from multiple urls
+    # loading_thread = threading.Thread(target=loading_animation)
+    # loading_thread.start()
+    # try:
+    #     data = get_summary()
+    # finally:
+    #     loading = False
+    #     loading_thread.join()
 
-    #Here we create a two-dimensional plot that has 2 rows and 3 comlums resulting in 6 charts in total
-    # r - rows, c - columns, n - iterator for setting the titles
-    r, c, n = 2, 3, 0
-    _, ax = plt.subplots(r,c, figsize=(20,10))
-    colors = plt.get_cmap('viridis')(np.linspace(0.9, 0.4, len(data)))
+    # #Here we create a two-dimensional plot that has 2 rows and 3 comlums resulting in 6 charts in total
+    # # r - rows, c - columns, n - iterator for setting the titles
+    # r, c, n = 2, 3, 0
+    # _, ax = plt.subplots(r,c, figsize=(20,10))
+    # colors = plt.get_cmap('viridis')(np.linspace(0.9, 0.4, len(data)))
 
-    titles = ['Top 5 industries','Work format', 'Work experience', 'Top 5 cities', 'Professional roles', 'Type of employment']
-    #Setting titles for each chart
-    for i in range(r):
-        for j in range(c):
-            ax[i,j].set_title(titles[n], fontsize=20, fontweight='bold')
-            n += 1
+    # titles = ['Top 5 industries','Work format', 'Work experience', 'Top 5 cities', 'Professional roles', 'Type of employment']
+    # #Setting titles for each chart
+    # for i in range(r):
+    #     for j in range(c):
+    #         ax[i,j].set_title(titles[n], fontsize=20, fontweight='bold')
+    #         n += 1
 
-    #Generating pie charts located in the first row
-    ax[0,0].pie(data[1].values(), labels=data[1].keys(), colors=colors, autopct='%.2f%%', textprops={'fontsize': 13})
-    ax[0,1].pie(data[5].values(), labels=data[5].keys(), colors=colors, autopct='%.2f%%', textprops={'fontsize': 13})
-    ax[0,2].pie(data[2].values(), labels=data[2].keys(), colors=colors, autopct='%.2f%%', textprops={'fontsize': 13})
-    #Generating bar charts located in the second row
-    ax[1,0].bar(data[0].keys(), data[0].values(), color=colors)
-    ax[1,1].bar(data[3].keys(), data[3].values(), color=colors)
-    ax[1,2].bar(data[4].keys(), data[4].values(), color=colors)
+    # #Generating pie charts located in the first row
+    # ax[0,0].pie(data[1].values(), labels=data[1].keys(), colors=colors, autopct='%.2f%%', textprops={'fontsize': 13})
+    # ax[0,1].pie(data[5].values(), labels=data[5].keys(), colors=colors, autopct='%.2f%%', textprops={'fontsize': 13})
+    # ax[0,2].pie(data[2].values(), labels=data[2].keys(), colors=colors, autopct='%.2f%%', textprops={'fontsize': 13})
+    # #Generating bar charts located in the second row
+    # ax[1,0].bar(data[0].keys(), data[0].values(), color=colors)
+    # ax[1,1].bar(data[3].keys(), data[3].values(), color=colors)
+    # ax[1,2].bar(data[4].keys(), data[4].values(), color=colors)
 
-    #Adjusting font size for bar charts
-    for i in range(3):
-        ax[1,i].tick_params(axis='x', labelsize=12)
+    # #Adjusting font size for bar charts
+    # for i in range(3):
+    #     ax[1,i].tick_params(axis='x', labelsize=12)
 
-    #Fixing the layout, so labels don't overlap and then saving the charts to png file
-    plt.tight_layout()
-    plt.savefig("charts.png")
+    # #Fixing the layout, so labels don't overlap and then saving the charts to png file
+    # plt.tight_layout()
+    # plt.savefig("charts.png")
 
-    #Here we count skills occurances from each vacancy URL, generate wordcloud and save it to png file
-    count_skills = Counter(get_skills())
-    wcloud = WordCloud(background_color='white', width=2000,height=1200).generate_from_frequencies(count_skills)
-    wcloud.to_file("word_cloud.png")
+    # #Here we count skills occurances from each vacancy URL, generate wordcloud and save it to png file
+    # count_skills = Counter(get_skills())
+    # wcloud = WordCloud(background_color='white', width=2000,height=1200).generate_from_frequencies(count_skills)
+    # wcloud.to_file("word_cloud.png")
 
-    visualization_requested = input("Would you like to generate a visualization in the form of charts to summarize the data? (y/n):")
-    if visualization_requested.lower() == 'y':
-        generate_pdf("charts.png", "word_cloud.png")
-    else:
-        print("If you want to analyze other vacancies, please rerun the program. Thank you.")
+    # visualization_requested = input("Would you like to generate a visualization in the form of charts to summarize the data? (y/n):")
+    # if visualization_requested.lower() == 'y':
+    #     generate_pdf("charts.png", "word_cloud.png")
+    # else:
+    #     print("If you want to analyze other vacancies, please rerun the program. Thank you.")
 
     print("done")
 
@@ -154,7 +155,7 @@ def get_roles():
         job_roles = json.load(f)
 
     #return '\n'.join(f"[{e['id']}]  {e['name']}" for e in job_roles["categories"][7]["roles"])
-    return [e for e in job_roles["categories"][7]["roles"]]
+    print([e for e in job_roles["categories"][7]["roles"]])
 
 #Reading data from local JSON: list of locations
 def get_areas():
