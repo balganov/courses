8d87ccfdb7b6a3b79f4d295204d14d16c0cd8d22
balganov <160471135+balganov@users.noreply.github.com>
2025-12-14 08:31:13 +0000

    Sun, Dec 14, 2025, 1:31 PM +05:00

--- cs50p/final_project/project.py
+++ cs50p/final_project/project.py
@@ -159,11 +159,10 @@ async def fetch_descriptions(vacancies):
         urls.append(i["url"])
 
     try:
-        async with aiohttp.ClientSession() as session:
-            for url in urls:
-                async with session.get(url) as response:
-                    json_data = await response.json()
-                    desc.append(json_data)
+        for url in urls:
+            async with session.get(url) as response:
+                json_data = await response.json()
+                desc.append(json_data)
     except aiohttp.ClientError as e:
         sys.exit(e)
     # try:
@@ -206,7 +205,8 @@ def get_summary():
         vacancies = json.load(f)
 
     #Create local JSON file with vacancy descriptions
-    asyncio.run(fetch_descriptions(vacancies))
+    async with aiohttp.ClientSession() as session:
+        asyncio.run(fetch_descriptions(vacancies))
     #Total number of vacancies
     #total = vacancies["found"]
 
