42d21a40ce8b0631db5b01f34a2ec870964e00fe
balganov <160471135+balganov@users.noreply.github.com>
2025-12-15 09:47:03 +0000

    Mon, Dec 15, 2025, 2:47 PM +05:00

--- cs50sql/snap/5.sql
+++ cs50sql/snap/5.sql
@@ -1,6 +1,9 @@
-SELECT *
+SELECT "friend_id"
 FROM "friends" AS "f"
 JOIN "users" AS "m" ON "f"."user_id" = "m"."id"
 WHERE "m"."username" = "lovelytrust487"
-OR "m"."username" = "exceptionalinspiration482"
-ORDER BY "friend_id";
+INTERSECT
+SELECT "friend_id"
+FROM "friends" AS "f"
+JOIN "users" AS "m" ON "f"."user_id" = "m"."id"
+WHERE "m"."username" = "exceptionalinspiration482";
