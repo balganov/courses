0c42a1be67fdf71618867d9d338435266ca597c4
balganov <160471135+balganov@users.noreply.github.com>
2025-12-22 04:09:21 +0000

    Mon, Dec 22, 2025, 9:09 AM +05:00

--- cs50p/final_project/check.py
+++ cs50p/final_project/check.py
@@ -2,13 +2,12 @@ import asyncio
 import json
 
 def main():
-    with open("vacancies.json", encoding="utf-8") as f:
-        vacancies = json.load(f)
+    # with open("vacancies.json", encoding="utf-8") as f:
+    #     vacancies = json.load(f)
 
-
-    urls = [i["url"] for i in vacancies["items"][:5]]
+    # urls = [i["url"] for i in vacancies["items"][:5]]
 
 def fetch_one():
-    
+
 
 main()
