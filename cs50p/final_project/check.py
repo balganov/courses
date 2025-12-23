31e59b4d601d16a611c7afb86b112b6896750508
balganov <160471135+balganov@users.noreply.github.com>
2025-12-23 07:13:31 +0000

    Tue, Dec 23, 2025, 12:13 PM +05:00

--- cs50p/final_project/check.py
+++ cs50p/final_project/check.py
@@ -7,22 +7,16 @@ async def main():
 
     # urls = [i["url"] for i in vacancies["items"][:5]]
     print("start")
-    task1 = asyncio.create_task(cor_func(5))
-    print("checkpoint1")
-    task2 = asyncio.create_task(cor_func(3))
+    task1 = asyncio.create_task(cor_func(100))
+    await asyncio.sleep(5)
+    print("here")
     print("checkpoint2")
     await task1
-    await task2
     print("done")
 
 async def cor_func(n):
 
     for _ in range(n):
-        if n==5:
-            print("hey")
-            await asyncio.sleep(1)
-        else:
-            print("---")
-            await asyncio.sleep(3)
+        print("hey")
 
 asyncio.run(main())
