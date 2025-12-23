619ae2953b98d8f1545ea4a4f0c6089cde7cbe59
balganov <160471135+balganov@users.noreply.github.com>
2025-12-23 09:56:30 +0000

    Tue, Dec 23, 2025, 2:56 PM +05:00

--- cs50p/final_project/check.py
+++ cs50p/final_project/check.py
@@ -2,12 +2,13 @@ import asyncio
 import json
 
 async def main():
-    # with open("vacancies.json", encoding="utf-8") as f:
-    #     vacancies = json.load(f)
+    with open("vacancies.json", encoding="utf-8") as f:
+        vacancies = json.load(f)
 
-    # urls = [i["url"] for i in vacancies["items"][:5]]
+    urls = [i["url"] for i in vacancies["items"][:10]]
+
     print("start")
-    task1 = asyncio.create_task(cor_func(120))
+    task1 = asyncio.create_task(cor_func(10))
     print("checkpoint2")
     await task1
     print("done")
@@ -16,8 +17,8 @@ async def cor_func(n):
 
     for k in range(n):
         print("hey")
-        if k % 30 == 0:
-            await asyncio.sleep(5)
-            print("waiting for 5 second")
+        if (k+1) % 2 == 0:
+            await asyncio.sleep(1.1)
+            print("waiting for 1.1 seconds")
 
 asyncio.run(main())
