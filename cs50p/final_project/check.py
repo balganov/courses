0642a8f34f3507d5e7778dca0eb0376b3c297dfd
balganov <160471135+balganov@users.noreply.github.com>
2025-12-23 06:15:32 +0000

    Tue, Dec 23, 2025, 11:15 AM +05:00

--- cs50p/final_project/check.py
+++ cs50p/final_project/check.py
@@ -1,11 +1,14 @@
 import asyncio
 import json
 
-async def main():
+def main():
     # with open("vacancies.json", encoding="utf-8") as f:
     #     vacancies = json.load(f)
 
     # urls = [i["url"] for i in vacancies["items"][:5]]
+    say_hi()
+
+async def say_hi():
     print("hello")
     await asyncio.sleep(3)
     print("world")
