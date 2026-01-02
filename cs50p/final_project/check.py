8b98ed0f4a83d8524e253b2a943bf20431be2da9
balganov <160471135+balganov@users.noreply.github.com>
2026-01-02 05:34:15 +0000

    Fri, Jan 2, 2026, 10:34 AM +05:00

--- cs50p/final_project/check.py
+++ cs50p/final_project/check.py
@@ -8,7 +8,8 @@ async def main():
             'https://api.hh.ru/vacancies/127949831?locale=EN&host=hh.ru', 'https://api.hh.ru/vacancies/127253831?locale=EN&host=hh.ru',
             'https://api.hh.ru/vacancies/128652740?locale=EN&host=hh.ru', 'https://api.hh.ru/vacancies/128675781?locale=EN&host=hh.ru',
             'https://api.hh.ru/vacancies/128739986?locale=EN&host=hh.ru', 'https://api.hh.ru/vacancies/128548168?locale=EN&host=hh.ru',
-            'https://api.hh.ru/vacancies/128734657?locale=EN&host=hh.ru', 'https://api.hh.ru/vacancies/128511589?locale=EN&host=hh.ru']
+            'https://api.hh.ru/vacancies/128734657?locale=EN&host=hh.ru', 'https://api.hh.ru/vacancies/128511589?locale=EN&host=hh.ru',
+            'https://api.hh.ru/vacancies/128511589?locale=EN&host=hh.ru']
     total_urls = len(urls)
     print("start")
     results = []
@@ -17,8 +18,8 @@ async def main():
     async with aiohttp.ClientSession() as session:
         async with asyncio.TaskGroup() as tg:
             for i in range(0,total_urls,2):
-                end = total_urls-i if total_urls-i < RATE_LIMIT else i
-                tasks = [tg.create_task(fetch_one(session, url, semaphore)) for url in urls[i:batch+RATE_LIMIT]]
+                end = total_urls-i if total_urls-i < RATE_LIMIT else i+RATE_LIMIT
+                tasks = [tg.create_task(fetch_one(session, url, semaphore)) for url in urls[i:end]]
                 print(tasks)
                 await asyncio.sleep(5)
 
