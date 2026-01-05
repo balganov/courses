81d92fb6b3217a4b00cbcef3c458e24736eac962
balganov <160471135+balganov@users.noreply.github.com>
2026-01-05 10:59:38 +0000

    Mon, Jan 5, 2026, 3:59 PM +05:00

--- cs50p/final_project/project.py
+++ cs50p/final_project/project.py
@@ -118,10 +118,9 @@ async def fetch_dictionaries():
 
 #Accessing list of vacancies filtered by location and roles
 async def fetch_vacancies(role_params, area_params):
-    access_token = 'APPLJFG7N22I3S8BBAE8ES7I573A8D4HBTF9P5FIQHNOJN12A5KGQ41VOLNI928K'
     header = {
         "User-Agent": "JobAnalyzer/1.0 (sdf010121@gmail.com)",
-        "Authorization": f"Bearer {access_token}"
+        "Authorization": f"Bearer APPLJFG7N22I3S8BBAE8ES7I573A8D4HBTF9P5FIQHNOJN12A5KGQ41VOLNI928K"
     }
     vacancy_params = {
         "professional_role":role_params,
@@ -198,10 +197,9 @@ async def fetch_descriptions(vacancies):
 
 #Fetching one url for asynchronious requests
 async def fetch_one(session, url, semaphore):
-    access_token = 'APPLJFG7N22I3S8BBAE8ES7I573A8D4HBTF9P5FIQHNOJN12A5KGQ41VOLNI928K'
     header = {
         "User-Agent": "JobAnalyzer/1.0 (sdf010121@gmail.com)",
-        "Authorization": f"Bearer {access_token}"
+        "Authorization": f"Bearer APPLJFG7N22I3S8BBAE8ES7I573A8D4HBTF9P5FIQHNOJN12A5KGQ41VOLNI928K"
     }
     async with semaphore:
         try:
