f266755c4ce1a441fc41453096a9333fdee953ff
balganov <160471135+balganov@users.noreply.github.com>
2026-01-05 09:47:46 +0000

    Mon, Jan 5, 2026, 2:47 PM +05:00

--- cs50p/final_project/token.py
+++ cs50p/final_project/token.py
@@ -1,16 +1,16 @@
-import requests
+# import requests
 
-url = "https://api.hh.ru/token"
-payload = {
-    'grant_type': 'authorization_code',
-    'client_id': 'IOC0UP207LQLRF0B0VELDH73D2C8J0CHK3RR353KDRSMJ6B54VJE4J4SCV0LDN3S',
-    'client_secret': 'G742S2A66RBRTQD4E67OFKL0GATHVSGA3PPDASP82T2D05E1QHJVK6IRPTM9NQV9',
-    'redirect_uri': 'http://localhost',
-    'code': 'RIPUKGILGC1DJHIJ785MA7RUEDF13JBA2112MTEGAMO201COGI42AE393AO4A06O'
-}
+# url = "https://api.hh.ru/token"
+# payload = {
+#     'grant_type': 'authorization_code',
+#     'client_id': 'IOC0UP207LQLRF0B0VELDH73D2C8J0CHK3RR353KDRSMJ6B54VJE4J4SCV0LDN3S',
+#     'client_secret': 'G742S2A66RBRTQD4E67OFKL0GATHVSGA3PPDASP82T2D05E1QHJVK6IRPTM9NQV9',
+#     'redirect_uri': 'http://localhost',
+#     'code': 'RIPUKGILGC1DJHIJ785MA7RUEDF13JBA2112MTEGAMO201COGI42AE393AO4A06O'
+# }
 
-response = requests.post(url, data=payload)
-print(response.json())
+# response = requests.post(url, data=payload)
+# print(response.json())
 
 # {
 #     'access_token': 'USERTUNGS70TSFPSNLDLS1PKIEM4GE2CCA51OI6F5UCK31UI476IJ7SBESK7AMQT',
