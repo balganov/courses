92c922aa717add0505892059f6824e09e1aae44e
balganov <160471135+balganov@users.noreply.github.com>
2026-01-05 09:38:54 +0000

    Mon, Jan 5, 2026, 2:38 PM +05:00

--- cs50p/final_project/token.py
+++ cs50p/final_project/token.py
@@ -5,7 +5,7 @@ payload = {
     'grant_type': 'authorization_code',
     'client_id': 'IOC0UP207LQLRF0B0VELDH73D2C8J0CHK3RR353KDRSMJ6B54VJE4J4SCV0LDN3S',
     'client_secret': 'G742S2A66RBRTQD4E67OFKL0GATHVSGA3PPDASP82T2D05E1QHJVK6IRPTM9NQV9',
-    'redirect_uri' = 'http://localhost',
+    'redirect_uri': 'http://localhost',
     'code': 'RIPUKGILGC1DJHIJ785MA7RUEDF13JBA2112MTEGAMO201COGI42AE393AO4A06O'
 }
 
