01e4de0b762022b328efee25c4512ec961911a5f
balganov <160471135+balganov@users.noreply.github.com>
2026-01-06 06:39:50 +0000

    Tue, Jan 6, 2026, 11:39 AM +05:00

--- cs50p/final_project/temp.py
+++ cs50p/final_project/temp.py
@@ -1,6 +1,6 @@
 
 
-titles = ['Top 5 industries','Work format', 'Work experience', 'Top 5 cities', 'Professional roles', 'Type of employment']
+titles = ['Top 5 cities', 'Top 5 industries', 'Work experience', 'Work format', 'Professional roles', 'Type of employment']
 data = [{'Almaty': 16, 'Astana': 3, 'Shymkent': 1, 'Kazakhstan': 0}, {'Financial Sector': 9, 'IT, System Integration, Internet': 5, 'Telecommunications, Communications': 4, 'Retail': 4, 'Medicine, Pharmaceuticals, Pharmacies': 1}, {'Between 1 and 3 years': 9, 'Between 3 and 6 years': 9, 'No experience': 2}, {'Product analyst': 0}, {'Full employment': 20}, {"At the employer's location": 13, 'Hybrid': 3, 'Remote': 1}]
 dict = {'Almaty': 16, 'Astana': 3, 'Shymkent': 1, 'Kazakhstan': 0}
 
