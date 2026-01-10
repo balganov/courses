3fbbb858d524f06ed7569865977cff983d0e38aa
balganov <160471135+balganov@users.noreply.github.com>
2026-01-10 02:59:15 +0000

    Sat, Jan 10, 2026, 7:59 AM +05:00

--- cs50p/final_project/temp.py
+++ cs50p/final_project/temp.py
@@ -22,3 +22,11 @@ for cluster in vacancies["clusters"]:
         summary_list.append(summary)
 
 print(summary_list)
+
+
+# [{'Minsk': 400, 'Gomel': 23, 'Vitebsk': 23, 'Brest': 16, 'Mogilev': 11},
+#  {'IT, System Integration': 356, 'Retail': 35, 'Financial Sector': 25, 'Electronics, Tool Engineering': 19, 'Media, Marketing': 15},
+#  {'Between 3 and 6 years': 201, 'Between 1 and 3 years': 181, 'No experience': 72, 'More than 6 years': 41},
+#  {'Programmer, developer': 476, 'Development team leader': 19},
+#  {'Full employment': 467, 'Part-time employment': 16, 'Project work': 12},
+#  {"At the employer's location": 269, 'Remote': 134, 'Hybrid': 117, 'Travel': 2}]
